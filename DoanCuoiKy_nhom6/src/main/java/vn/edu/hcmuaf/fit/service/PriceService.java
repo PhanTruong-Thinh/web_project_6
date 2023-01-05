0b11fc28c4c2d499dfaa244f4287023cd0559c70
PhanTruong-Thinh <20130181@st.hcmuaf.edu.vn>
2023-01-06 02:50:43 +0700

    cap nhat

---
+++ DoanCuoiKy_nhom6/src/main/java/vn/edu/hcmuaf/fit/service/PriceService.java
@@ -0,0 +1,30 @@
+package vn.edu.hcmuaf.fit.service;
+
+import vn.edu.hcmuaf.fit.db.JDBIConnector;
+import vn.edu.hcmuaf.fit.model.Product;
+import java.time.LocalDate;
+import java.util.stream.Collectors;
+
+public class PriceService {
+
+    //them san pham
+    public static void addPricePro(String maSP, double price, double giaBan){
+        JDBIConnector.get().withHandle(handle -> {
+            return handle.createQuery("insert  into  gia_sanpham values ('"+maSP+"',"+LocalDate.now()+","+price+","+giaBan+")").mapToBean(Product.class).collect(Collectors.toList());
+        });
+    }
+
+    //xoa san pham
+    public static void deletePricePro(String maSP){
+        JDBIConnector.get().withHandle(handle -> {
+            return handle.createQuery("delete from gia_sanpham where Ma_SP='"+maSP+"'").mapToBean(Product.class).collect(Collectors.toList());
+        });
+    }
+
+    //cap nhat gia ban
+    public static void updatePricePro(String maSP,double price){
+        JDBIConnector.get().withHandle(handle -> {
+            return handle.createQuery("update gia_sanpham set Ngay="+LocalDate.now()+",Gia_Ban="+price+" where Ma_SP='"+maSP+"'").mapToBean(Product.class).collect(Collectors.toList());
+        });
+    }
+}
